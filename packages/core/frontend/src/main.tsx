1ddd0ba09c5cfa583433557cea443eec938575d6
Kirill <marchenko.kiri11@yandex.ru>
2025-05-13 00:10:45 +0300

    chore: add reset styles

--- packages/core/frontend/src/main.tsx
+++ packages/core/frontend/src/main.tsx
@@ -1,6 +1,10 @@
+import "@radix-ui/themes/layout/components.css";
+import "@radix-ui/themes/layout/tokens.css";
+import "@radix-ui/themes/layout/utilities.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./base/normalize.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
