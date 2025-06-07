6133a3effb52304e07424578bd60ed5969e91eef
Kirill <marchenko.kiri11@yandex.ru>
2025-06-07 17:57:45 +0300

    feat: create logic for docker

--- packages/core/frontend/vite.config.ts
+++ packages/core/frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: Number(process.env.CLIENT_PORT) || 3000,
+    host: "0.0.0.0",
+    port: Number(process.env.DEV_PORT) || 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   plugins: [react()],
 });
