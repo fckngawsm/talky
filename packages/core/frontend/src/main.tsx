a60d4aeafc5cc690b07dcd171ace8e1bc15d18c1
Kirill <marchenko.kiri11@yandex.ru>
2025-06-10 01:37:17 +0300

    feat: install react tanstack query

--- packages/core/frontend/src/main.tsx
+++ packages/core/frontend/src/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routerConfig } from "./providers/router/router.config";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={routerConfig} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routerConfig} />
+    </QueryClientProvider>
   </StrictMode>,
 );
