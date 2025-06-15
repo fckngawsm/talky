2bd99caed6366e8e2b1c810b93f0809edf293ab6
Kirill <marchenko.kiri11@yandex.ru>
2025-06-15 13:36:07 +0300

    feat: update imports

--- packages/shared/nats-module/src/index.ts
+++ packages/shared/nats-module/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./nats-transport.module";
 // patterns
-export * from "./patterns/index";
+export * from "./patterns/patterns";
 // contacts
-export * from "./contracts/index";
+export * from "./contracts/contracts";
