d6040b73551d10436688eb0b17f7629312a29574
Kirill <marchenko.kiri11@yandex.ru>
2025-06-17 23:47:02 +0300

    wip: need fix prisma

--- packages/core/user-service/src/app.module.ts
+++ packages/core/user-service/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
-  imports: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
