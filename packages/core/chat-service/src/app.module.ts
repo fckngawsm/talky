175d86e0e94d595c45295c834302a5193498151f
Kirill <marchenko.kiri11@yandex.ru>
2025-07-05 23:43:42 +0300

    feat: connect to postgres

--- packages/core/chat-service/src/app.module.ts
+++ packages/core/chat-service/src/app.module.ts
@@ -1,5 +1,21 @@
 import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_NAME"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: configService.get("NODE_ENV") === "development",
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
