9fa97e354ff36ddb6e02e06ab5daafa46937a4c3
Kirill <marchenko.kiri11@yandex.ru>
2025-07-09 19:25:37 +0300

    fix: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,14 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: [
+      './tsconfig.json',
+      './packages/core/chat-service/tsconfig.json',
+      './packages/core/api/tsconfig.json',
+      './packages/core/auth-service/tsconfig.json',
+      './packages/core/user-service/tsconfig.json',
+      './packages/core/frontend/tsconfig.json',
+    ],
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
