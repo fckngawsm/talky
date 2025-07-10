94c6224b3fdaff525c1c7d243d6d8e5117abe256
Kirill <marchenko.kiri11@yandex.ru>
2025-07-11 00:22:07 +0300

    feat: update init env fn

--- init.ts
+++ init.ts
@@ -1,4 +1,18 @@
-const fs = require('fs')
+const fs = require("fs");
 
-fs.copyFileSync('.env.sample', '.env')
+fs.copyFileSync(".env.sample", ".env");
 
+fs.copyFileSync("packages/core/api/.env.sample", "packages/core/api/.development.env");
+fs.copyFileSync(
+  "packages/core/auth-service/.env.sample",
+  "packages/core/auth-service/.development.env",
+);
+fs.copyFileSync(
+  "packages/core/chat-service/.env.sample",
+  "packages/core/chat-service/.development.env",
+);
+fs.copyFileSync(
+  "packages/core/user-service/.env.sample",
+  "packages/core/user-service/.development.env",
+);
+fs.copyFileSync("packages/core/frontend/.env.sample", "packages/core/frontend/.development.env");
