ef13685054306cb78574c809760f2e9238abc149
Kirill <marchenko.kiri11@yandex.ru>
2025-07-11 20:17:04 +0300

    wip: need create ui for chat and members

--- packages/core/api/src/modules/chat/chat.controller.ts
+++ packages/core/api/src/modules/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import { CookieAuthGuard } from "src/common/guards/cookie-auth.guard";
 import { ChatService } from "./chat.service";
 import { ChatDto } from "./dto/chat.dto";
@@ -12,7 +12,11 @@ export class ChatController {
   getCurrentUser() {}
 
   @Post("")
-  createChat(@Body() dto: ChatDto) {
-    return this.chatService.createChat(dto);
+  createChat(@Body() dto: ChatDto, @Req() req) {
+    const memberIds = [req.user.id, ...dto.memberIds];
+    return this.chatService.createChat({
+      ...dto,
+      memberIds,
+    });
   }
 }
