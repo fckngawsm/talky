0e5d183cbe65034c5eb235a7f166736f99cd5bfd
Kirill <marchenko.kiri11@yandex.ru>
2025-07-13 22:22:06 +0300

    feat: add messages

--- packages/core/frontend/src/features/chat-selected/chat-messages.ui.tsx
+++ packages/core/frontend/src/features/chat-selected/chat-messages.ui.tsx
@@ -1,11 +1,14 @@
+import { useChat } from "@/providers/store/chat.store";
+
 export const MessageItem = ({ content }) => {
   return <li className="message__item">{content}</li>;
 };
 
 export const MessageList = () => {
+  const { selectedChat } = useChat();
   return (
     <ul className="chat__messages">
-      {[].map((m, index) => (
+      {selectedChat?.messages.map((m, index) => (
         <MessageItem key={index} content={m.content} />
       ))}
     </ul>
